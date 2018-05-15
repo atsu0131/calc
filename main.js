7369f21da0a9a6f7d16cde87b00a9bba1d4d0e63
atsushi.isai <ec2-user@ip-172-31-34-236.us-east-2.compute.internal>
2018-05-15 15:26:10 +0000

    second

---
+++ main.js
@@ -0,0 +1,37 @@
+function getNum1() {
+  // num1の数値を戻り値としてreturnする処理を書いてください
+   var num1 = parseFloat(document.getElementById("num1").value);
+   return num1;
+}
+
+function getNum2() {
+  // num2の数値を戻り値としてreturnする処理を書いてください
+    var num2 = parseFloat(document.getElementById("num2").value);
+   return num2;
+}
+
+
+function showResult(num) {
+  // <div id="box">にnumを表示する処理を書いてください
+  document.getElementById("box").innerHTML = num;
+}
+
+document.getElementById("add-button").onclick = function() {
+  var result = getNum1() + getNum2();
+  showResult(result);
+};
+
+document.getElementById("sub-button").onclick = function() {
+  var result = getNum1() - getNum2();
+  showResult(result);
+};
+
+document.getElementById("mul-button").onclick = function() {
+  var result = getNum1() * getNum2();
+  showResult(result);
+};
+
+document.getElementById("div-button").onclick = function() {
+  var result = getNum1() / getNum2();
+  showResult(result);
+};
